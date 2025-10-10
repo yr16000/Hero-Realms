93746d9cf3976dbb54bbc07b726b8d937ecee608
Yanis <yanis.et.co04@gmail.com>
2025-10-10 10:23:40 +0200

    Ajout logique Player et Game

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,7 +1,20 @@
 #ifndef GAME_HPP
 #define GAME_HPP
+#include "Player.hpp"
 
 class Game{
+    private:
+        Player players[2];
+        std::vector<std::unique_ptr<Carte>> marche;
+        std::vector<std::unique_ptr<Carte>> pioche;
+    public:
+        Game();
+        void demarrerPartie();
+        void tourDeJeu(Player& joueur);
+        void finDePartie();
+        void afficherMarche() const;
+        Carte* acheterCarte(int index, Player& acheteur);
+        void ajouterCarteMarche(std::unique_ptr<Carte> carte);
 
 };
 
--- include/Player.hpp
+++ include/Player.hpp
@@ -1,11 +1,37 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
+#include <vector>
+#include <memory>
+#include "Carte.hpp"
 
 class Player{
     private:
         int id;
-        int hp;
-        
+        int hp=50;
+        int atk=0;
+        int gold=0;
+        int heal=0;
+        std::vector<std::unique_ptr<Carte>> main;
+        std::vector<std::unique_ptr<Carte>> deck;
+        std::vector<std::unique_ptr<Carte>> defausse;
+    public:
+        Player(int id);
+        void piocherCarte(int nb);
+        void acheterCarte(int index, Game& game);
+        void modiffGold(int nb);
+        void modiffCombat(int nb);
+        void modiffHeal(int nb);
+        void soigner(int nb);
+        void attaquer(Player& cible, int nb,Carte* carte=nullptr);
+        void subirDegat(int nb);
+        void jouerCarte(int index, Game& game);
+        void sacrifierCarte(Carte* carte);
+        bool estVivant() const;
+        int getHp() const;
+        int getGold() const;
+        int getAtk() const;
+        void afficherMain() const;
+        void afficherStats() const;
 };
 
 
